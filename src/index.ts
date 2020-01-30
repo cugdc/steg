23f674f54399e13f7a80be3ed610df2a4ecbdf96
Joshua Franklin <joshuafranklin26@gmail.com>
2020-01-29 19:56:48 -0700

    Add bookshelf on desk scene and back button on library scene

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ function init() {
   switchScene(Scene.DESK);
   onClick('#scene-menu .play-button',   () => switchScene(Scene.DESK));
   onClick('#scene-desk .ink-and-quill', () => switchScene(Scene.MENU));
+  onClick('#scene-desk .bookshelf', () => switchScene(Scene.LIBRARY));
+  onClick('#scene-library .back-button', () => switchScene(Scene.DESK));
 }
 
 init();
